ce140d42f703d38adb28fe5a0af32b1ad40b149f
SAURABH MISHRA <envirolytics.official@gmail.com>
2026-09-21 16:51:32 +0530

    Master Fix V15: expose calculated DO saturation

--- frontend/src/pages/WaterQuality.jsx
+++ frontend/src/pages/WaterQuality.jsx
@@ -506,6 +506,7 @@ const WaterQuality = () => {
                       isCustomVideo={Boolean(vidPath)}
                       temperatureC={typeof currentValues.TEMPER === 'number' ? currentValues.TEMPER : null}
                       saturationPct={typeof currentValues.DO_SATURATION === 'number' ? currentValues.DO_SATURATION : null}
+                      saturationVendorPct={typeof currentValues.DO_SATURATION_VENDOR === 'number' ? currentValues.DO_SATURATION_VENDOR : null}
                     />
                     <AerationVideoUploader
                       hardwareId={selectedHw}
